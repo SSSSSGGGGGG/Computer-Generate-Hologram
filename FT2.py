0eefaa28ef4ffb4d6470dd520b22b3847c3b1105
SSSSSGGGGGG <sgao@umh.es>
2024-07-26 16:12:52 +0200

    cal of angle and convert it to color

--- FT2.py
+++ FT2.py
@@ -28,13 +28,21 @@ im_gray=color.rgb2gray(im) # need to check
 
 im_r_fft=fft2(im_r)
 I_r=im_r_fft**2
-im_r_if=ifft2(im_r_fft)
-# plt.imshow(abs(I))
-# plt.axis('off')
+
+angle=np.angle(im_r_fft)
+# the calibration between red and gray level 1.85-->255
+interval=255/(1.85*np.pi)
+angle_0_2pi = np.mod(angle, 2 * np.pi)
+angle_0_2pi =angle_0_2pi*interval
+
+im_r_if=ifft2(angle)
+plt.imshow(abs(im_r_if))
+plt.axis('off')
 im_new=np.zeros_like(im)
-im_new[:,:,0]=im_r_fft
+im_new[:,:,0]=angle_0_2pi
 im_new_array = im_new.astype(np.uint8)
 
 im_new_t = Image.fromarray(im_new_array)
 im_new_t.save("FT of car.png")
 im_new_t.show()
+
