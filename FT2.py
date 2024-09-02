0562f2dadcf9bac88f860b2f2255597ded40e599
SSSSSGGGGGG <sgao@umh.es>
2024-09-02 15:55:50 +0200

    latest

--- FT2.py
+++ FT2.py
@@ -13,14 +13,11 @@ import os
 from skimage import color
 
 
-<<<<<<< HEAD
+
+
 os.chdir("C:/Users/Laboratorio/MakeHologram/OriginalImage")
-filename="ele_half.png"
-=======
-os.chdir("C:/Users/gaosh/Documents/python/Digital-hologram/OriginalImage")
-filename="1.jpg"
->>>>>>> 6ea46742f7ffac311c80328b5db84c55737af1cb
-im=plt.imread(filename)[:,:,:3]
+filename="1"
+im=plt.imread(f"{filename}.jpg")
 shift_state="shift" # to check the shift influence,
 
 im_r=im[:,:,0]
@@ -33,30 +30,30 @@ im_b=im[:,:,2]
 
 im_r_process=(im_r-1)*(-1)*255
 # im_r_fft=fftshift(fft2(im_r))
-<<<<<<< HEAD
-im_r_fft=fftshift(ifft2(im_r_process))
-=======
+
+# im_r_fft=fftshift(ifft2(im_r_process))
+
 im_r_fft=fftshift(fft2(im_r))
 im_g_fft=fftshift(fft2(im_g))
 im_b_fft=fftshift(fft2(im_b))
->>>>>>> 6ea46742f7ffac311c80328b5db84c55737af1cb
 
-rows, cols = im_r.shape
-# Frequency coordinates
-u = np.fft.fftshift(np.fft.fftfreq(rows))  # Frequency coordinates along rows
-v = np.fft.fftshift(np.fft.fftfreq(cols))  # Frequency coordinates along columns
-# print(u*rows,v*cols)
-# Create frequency grid
-U, V = np.meshgrid(u, v)
+
+# rows, cols = im_r.shape
+# # Frequency coordinates
+# u = np.fft.fftshift(np.fft.fftfreq(rows))  # Frequency coordinates along rows
+# v = np.fft.fftshift(np.fft.fftfreq(cols))  # Frequency coordinates along columns
+# # print(u*rows,v*cols)
+# # Create frequency grid
+# U, V = np.meshgrid(u, v)
 
 #(-pi, pi]
 phase = np.angle(im_r_fft)+np.pi
-<<<<<<< HEAD
+
 mag=np.abs(im_r_fft)
-=======
+
 phase_g = np.angle(im_g_fft)+np.pi
 phase_b = np.angle(im_b_fft)+np.pi
->>>>>>> 6ea46742f7ffac311c80328b5db84c55737af1cb
+
 # the calibration between red and gray level 1.85-->255
 interval=255/(2*np.pi)
 angle_0_pi = phase*interval
@@ -70,36 +67,22 @@ angle_0_pi_b = phase_b*interval_b
 # angle_0_pi =angle_0_pi # this is the one in 0-255
 plt.figure(3)
 plt.imshow(phase,cmap="hot")
-plt.savefig("holo.tif")
+
 plt.show()
 
-<<<<<<< HEAD
-plt.figure(1)
-plt.imshow(mag,cmap="hot")
-plt.savefig("holo.tif")
-plt.show()
-
-im_new=np.zeros_like(im)
-im_new[:,:,0]=angle_0_pi
-=======
-
-plt.figure(2)
-# plt.imshow(np.log(1+abs(im_r_fft)),cmap="gray")
-plt.imshow(phase_g,cmap="gray",extent=[v.min(), v.max(), u.min(), u.max()], origin='lower', aspect='auto')
-plt.show()
 
 im_new=np.zeros_like(im)
 im_new[:,:,0]=np.mod(angle_0_pi,255)
 im_new[:,:,1]=np.mod(angle_0_pi_g,255)
 im_new[:,:,2]=np.mod(angle_0_pi_b,255)
->>>>>>> 6ea46742f7ffac311c80328b5db84c55737af1cb
+
 im_new_array = im_new.astype(np.uint8)
 
 im_new_t = Image.fromarray(im_new_array)
-im_new_t.save(f"ft of ele{interval}.png")
+im_new_t.save(f"{filename}_ft.png")
 
-<<<<<<< HEAD
-im_saved=plt.imread(f"ft of ele{interval}.png")[:,:,0]*255
+
+im_saved=plt.imread(f"{filename}_ft.png")[:,:,0]*255
 phase_2=im_saved/interval
 
 # combined = np.exp(1j * phase)
@@ -108,7 +91,6 @@ im_r_if = fft2(np.exp(1j * phase_2))
 # I_r=im_r_if*im_r_if.conjugate()
 plt.figure(2)
 plt.imshow(np.abs(im_r_if),cmap="hot")
-=======
 
 im_saved=plt.imread("C:/Users/gaosh/Documents/python/Digital-hologram/OriginalImage/ft_of_1.jpg_resized.png")*255
 # phase_2=im_saved/interval
@@ -119,9 +101,4 @@ plt.show()
 # combined = np.exp(1j * phase)
 im_r_if = ifft2(fftshift(np.exp(1j * phase_g)))
 
-# I_r=im_r_if*im_r_if.conjugate()
-plt.figure(1)
-plt.imshow(im_new,cmap="hot",extent=[v.min(), v.max(), u.min(), u.max()], origin='lower', aspect='auto')
->>>>>>> 6ea46742f7ffac311c80328b5db84c55737af1cb
-plt.show()
-plt.savefig(f"ift of {filename} {interval}.tif")
+
--- FT3.py
+++ FT3.py
@@ -32,36 +32,36 @@ im_new_t = Image.fromarray(im_new_array)
 # im_new_t.show()
 im_new_t.save(f"{filename}_RGBholo.png")
 
-im_rgb_ift=ifft2(fftshift(np.exp(1j*phase_rgb)))*255
+im_rgb_ift=ifft2(np.exp(1j*phase_rgb))
 
 plt.figure(2)
 plt.imshow(abs(im_rgb_ift))
 plt.title("iFT")
 plt.show()
-# the following is for red chennel holo
-im_r=im[:,:,0]
-im_r_ft=fftshift(fft2(im_r))
-phase_r=np.angle(im_r_ft)
+# # the following is for red chennel holo
+# im_r=im[:,:,0]
+# im_r_ft=fftshift(fft2(im_r))
+# phase_r=np.angle(im_r_ft)
 
-plt.figure(3)
-plt.imshow(phase_r)
-plt.title("FT of R chennel phase")
-plt.show()
+# plt.figure(3)
+# plt.imshow(phase_r)
+# plt.title("FT of R chennel phase")
+# plt.show()
 
-im_new_r=np.zeros_like(im)
-im_new_r[:,:,0] = phase_r
-im_new_r = Image.fromarray(im_new_r.astype(np.uint8))
-im_new_r.save(f"{filename}_Rholo.png")
+# im_new_r=np.zeros_like(im)
+# im_new_r[:,:,0] = phase_r
+# im_new_r = Image.fromarray(im_new_r.astype(np.uint8))
+# im_new_r.save(f"{filename}_Rholo.png")
 
-im_new_gray=np.zeros_like(im)
-im_new_gray[:,:,0] = phase_r
-im_new_gray[:,:,1] = phase_r
-im_new_gray[:,:,2] = phase_r
-im_new_gray = Image.fromarray(im_new_gray.astype(np.uint8))
-im_rgb_gray=im_new_gray.convert('L')
-im_rgb_gray.save(f"{filename}_grayholo.png")
+# im_new_gray=np.zeros_like(im)
+# im_new_gray[:,:,0] = phase_r
+# im_new_gray[:,:,1] = phase_r
+# im_new_gray[:,:,2] = phase_r
+# im_new_gray = Image.fromarray(im_new_gray.astype(np.uint8))
+# im_rgb_gray=im_new_gray.convert('L')
+# im_rgb_gray.save(f"{filename}_grayholo.png")
 
-plt.figure(4)
-plt.imshow(im_new_gray)
-plt.title("FT of gray phase")
-plt.show()
+# plt.figure(4)
+# plt.imshow(im_new_gray)
+# plt.title("FT of gray phase")
+# plt.show()
