c3e118727d628f31ae8b9f34e1c5828d53603387
SSSSSGGGGGG <sgao@umh.es>
2024-10-18 13:39:36 +0200

    demo

--- FFT_demos/FFT_demos.py
+++ FFT_demos/FFT_demos.py
@@ -4,13 +4,14 @@ Created on Wed Feb 14 11:28:11 2024
 
 @author: SG
 """
-from PIL import Image
+
 import numpy as np
-import scipy as sp
-from scipy.fft import fft2, fftshift,ifft2,ifftshift
+from scipy.fft import fft2, fftshift
 import matplotlib.pyplot as plt
-import os
-from skimage import color
+
+"""
+We define a 2D array with the size of 512*512 pixels, which is filled with 0 as background (in black color). We draw a small square with the size of 20*20 pixels that is filled with a value of 1 (in white color) in the array's center. The 2D FFT algorithm is applied for all the shapes, and "fftshift" is applied as well for shifting the low frequency to the center in the frequency domain. Finally, the magnitudes of FFT for different shapes are saved.
+"""
 
 square=np.zeros((512,512))
 width,height=square.shape
@@ -21,8 +22,6 @@ plt.imsave("square.png", square,cmap='gray')
 
 square_fft=fftshift(fft2(square))
 square_mag=abs(square_fft)/np.max(abs(square_fft))
-# plt.imshow(rect_mag,cmap="hot")
-# plt.colorbar()
 plt.imsave("square_fft.png", square_mag,cmap='hot')
 
 rect=np.zeros((512,512))
@@ -35,10 +34,8 @@ plt.imsave("rect_fft.png", rect_fft_mag,cmap='hot')
 
 slit=np.zeros((512,512))
 slit[:,int(center)-int(1*width_c):int(center)+int(1*width_c)]=1
-plt.imsave("bar.png", slit,cmap='gray')
+plt.imsave("slit.png", slit,cmap='gray')
 
 slit_fft=fftshift(fft2(slit))
 slit_mag=abs(slit_fft)/np.max(abs(slit_fft))
-plt.imsave("bar_fft.png", slit_mag,cmap='hot')
-# plt.imshow(bar_mag,cmap="hot")
-# plt.colorbar()
+plt.imsave("slit_fft.png", slit_mag,cmap='hot')
--- FFT_demos/FFTofSLM.py
+++ FFT_demos/FFTofSLM.py
@@ -4,55 +4,49 @@ Created on Wed Feb 14 11:28:11 2024
 
 @author: SG
 """
-from PIL import Image
+
 import numpy as np
-import scipy as sp
-from scipy.fft import fft2, fftshift,ifft2,ifftshift
+from scipy.fft import fft2, fftshift
 import matplotlib.pyplot as plt
-import os
-from skimage import color
+
 
 square=np.zeros((512,512))
 width,height=square.shape
 center=int(width/2)
-width_c=30
+width_c=32
+period=16
+half_period=int(period/2)
 square[center-width_c:center+width_c,center-width_c:center+width_c]=1
-for i in range(center - width_c, center + width_c, 12):  # Step of 12 within the block
-    # print(i)
-    square[center - width_c:center + width_c, i:i + 6] = 0  # Apply to the defined region only
-
-# plt.imsave("slits in square.png", square,cmap='gray')
+for i in range(center - width_c, center + width_c, period):  
+    square[center - width_c:center + width_c, i:i + half_period] = 0  
+plt.imsave(f"Vertical slits p_{period}.png", square,cmap='gray')
 
 square_fft=fftshift(fft2(square))
 square_mag=abs(square_fft)/np.max(abs(square_fft))
-# plt.imsave("slits in square_fft.png", square_mag,cmap='hot')
-# plt.imshow(square_mag,cmap="hot")
-# plt.colorbar()
+plt.imsave(f"FFT_Vslits p_{period}.png", square_mag,cmap='hot')
+
 square_h=np.zeros((512,512))
 width_h,height_h=square_h.shape
 center_h=int(width_h/2)
-# width_c=30
 square_h[center_h-width_c:center_h+width_c,center_h-width_c:center_h+width_c]=1
-for i in range(center_h-width_c, center_h+width_c, 12):  # Step of 12 within the block
-    # print(i)
-    square_h[i:i + 6,center_h - width_c:center_h+width_c] = 0  # Apply to the defined region only
-# plt.imsave("slits_h in square.png", square_h,cmap='gray')        
+for i in range(center_h-width_c, center_h+width_c, period):  
+    square_h[i:i + half_period,center_h - width_c:center_h+width_c] = 0  
+plt.imsave(f"Horizontal slits p_{period}.png", square_h,cmap='gray')        
 
 square_h_fft=fftshift(fft2(square_h))
 square_h_mag=abs(square_h_fft)/np.max(abs(square_h_fft))
-# plt.imsave("slits_h in square_fft.png", square_h_mag,cmap='hot')
-
+plt.imsave(f"FFT_Hslits p_{period}.png", square_h_mag,cmap='hot')
 
 SLM=np.zeros((512,512))
 width_s,height_s=SLM.shape
 center_s=int(width_s/2)
 SLM[center_s-width_c:center_s+width_c,center_s-width_c:center_s+width_c]=1
-for i in range(center_h-width_c, center_h+width_c, 12):  # Step of 12 within the block
-    # print(i)
-    SLM[i:i + 6,center_s - width_c:center_s+width_c] = 0 
-    SLM[center - width_c:center + width_c, i:i + 6] = 0
-# plt.imsave("SLM.png", SLM,cmap='gray')
+for i in range(center_h-width_c, center_h+width_c, period):  
+    SLM[i:i + half_period,center_s - width_c:center_s+width_c] = 0 
+    SLM[center - width_c:center + width_c, i:i + half_period] = 0
+plt.imsave(f"SLM p_{period}.png", SLM,cmap='gray')
 
 SLM_fft=fftshift(fft2(SLM))
 SLM_mag=abs(SLM_fft)/np.max(abs(SLM_fft))
-plt.imsave("SLM_fft.png", SLM_mag,cmap='hot')
+plt.imsave(f"FFT_SLM p_{period}.png", SLM_mag,cmap='hot')
+# plt.imshow(SLM_mag,cmap="hot")
