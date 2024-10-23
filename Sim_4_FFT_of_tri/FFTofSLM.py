4a7ced6e6d6a916b1b20663a7edfb4b96fc0d119
SSSSSGGGGGG <sgao@umh.es>
2024-10-23 15:10:43 +0200

    phase gratings are done

--- Sim_4_FFT_of_tri/FFTofSLM.py
+++ Sim_4_FFT_of_tri/FFTofSLM.py
@@ -12,90 +12,98 @@ import matplotlib.pyplot as plt
 
 Rslits=np.ones((1080,1920))
 height,width=Rslits.shape
-center_h=int(height/2)
-center_w=int(width/2)
-period=128*4.6e-6
+period=64*4.6e-6
 half_period=int(period/2)
-phase=0.63*np.pi
-# for i in range(0, width, 2*period):  
-#     # print(i,i +period)
-#     Rslits[0:height,i:i +period]=np.exp(1j*phase)  
-# # plt.imsave(f"Vertical slits p_{period}.png",Rslits,cmap='Reds')
+phase=0.637*np.pi
+period_X=92*4.6e-6
+period_Y=400*4.6e-6
 
-# Rslits_fft=fftshift(fft2(Rslits))
-# Rslits_mag=abs(Rslits_fft)/np.max(abs(Rslits_fft))
-# plt.imsave(f"FFT_Vslits p_{period}.png", Rslits_mag,cmap='hot')
-# Create a spatial grid
-x = np.linspace(0, width*4.6e-6, width)
-y = np.linspace(0, height*4.6e-6, height)
+x = np.linspace(-0.5*width*4.6e-6, 0.5*width*4.6e-6, width)
+y = np.linspace(-0.5*height*4.6e-6, 0.5*height*4.6e-6, height)
 X, Y = np.meshgrid(x, y)
-beam_width = 200  # Width of the Gaussian beam
 
-# Create a sinusoidal phase grating
-phase_grating = np.exp(1j * phase * np.sin(2 * np.pi * X / period))
+binary_phase = np.where((X // period) % 2 == 0, 1, np.exp(1j *phase))
 
-amplitude = np.exp(-((X/4.6e-6 - center_w)**2 + (Y/4.6e-6 - center_h)**2) / (2 * beam_width**2))
-# Compute the 2D Fourier Transform and shift zero frequency component to the center
-fft_grating = fftshift(fft2(amplitude*phase_grating))
+blazed_phase_X = ((2  / period_X) * ((X) %  (period_X)))
+blazed_phase_offset_X=blazed_phase_X
+blazed_grating_X = np.exp(1j *blazed_phase_offset_X*np.pi)#np.mod(blazed_phase, 2 * np.pi)
 
-# Compute the magnitude of the Fourier Transform
-fft_magnitude = np.abs(fft_grating)
+blazed_phase_Y = ((2 / period_Y) * ((Y) %  (period_Y)))
+blazed_phase_offset_Y=blazed_phase_Y
+blazed_grating_Y = np.exp(1j *blazed_phase_offset_Y*np.pi)
+
+beam_width = 200*4.6e-6
+Guassia_amplitude = np.exp(-((X - 0*4.6e-6)**2 + (1*Y - 0)**2) / (2 * beam_width**2))
+Guassia_amplitude=Guassia_amplitude/np.max(Guassia_amplitude)
+
+fft_grating_binary = fftshift(fft2(binary_phase*Guassia_amplitude))
+I_binary=np.abs(fft_grating_binary)**2
+sumI=np.sum(I_binary)
+I_binary_nor=I_binary/sumI
+I_max=np.max(I_binary)
+Index_max = np.unravel_index(np.argmax(I_binary), I_binary.shape)
+
+fft_grating_withBlazed = fftshift(fft2(blazed_grating_X*blazed_grating_Y*binary_phase*Guassia_amplitude))
+I_withBlazed=np.abs(fft_grating_withBlazed)**2
+sumI_withBlazed=np.sum(I_withBlazed)
+I_withBlazed_nor=I_withBlazed/sumI_withBlazed
+
 # Define the center region to crop
 center_h = height // 2
 center_w = width // 2
-crop_size = 200  # Size of the cropped region (200x200 pixels)
-
+crop_size_h = 50  # Size of the cropped region (200x200 pixels)
+crop_size_w = 200
 # Crop the FFT magnitude
-start_h = center_h - crop_size // 2
-end_h = center_h + crop_size // 2
-start_w = center_w - crop_size // 2
-end_w = center_w + crop_size // 2
-fft_magnitude_cropped = fft_magnitude[start_h:end_h, start_w:end_w]
-# Normalize the magnitude for visualization
-# fft_magnitude = fft_magnitude / np.max(fft_magnitude)
-# square_h=np.zeros((512,512))
-# width_h,height_h=square_h.shape
-# center_h=int(width_h/2)
-# square_h[center_h-width_c:center_h+width_c,center_h-width_c:center_h+width_c]=1
-# for i in range(center_h-width_c, center_h+width_c, period):  
-#     square_h[i:i + half_period,center_h - width_c:center_h+width_c] = 0  
-# plt.imsave(f"Horizontal slits p_{period}.png", square_h,cmap='gray')        
+start_h = center_h - crop_size_h // 2
+end_h = center_h + crop_size_h // 2
+start_w = center_w - crop_size_w // 2
+end_w = center_w + crop_size_w // 2
+I_cropped_binary = I_binary_nor[start_h:end_h, start_w:end_w]
+I_cropped_withBlazed = I_withBlazed_nor[start_h:end_h, start_w:end_w]
 
-# square_h_fft=fftshift(fft2(square_h))
-# square_h_mag=abs(square_h_fft)/np.max(abs(square_h_fft))
-# plt.imsave(f"FFT_Hslits p_{period}.png", square_h_mag,cmap='hot')
+frequencies_x = np.fft.fftshift(np.fft.fftfreq(width, d=1))
+frequencies_y = np.fft.fftshift(np.fft.fftfreq(height, d=1))
+# print(I_max,sumI)
+# plt.figure()
+# plt.imshow(np.real(binary_phase), cmap='gray')
+# plt.title('Real part of the binary phase grating')
+# plt.colorbar()
+# plt.show()
 
-# SLM=np.zeros((512,512))
-# width_s,height_s=SLM.shape
-# center_s=int(width_s/2)
-# SLM[center_s-width_c:center_s+width_c,center_s-width_c:center_s+width_c]=1
-# for i in range(center_h-width_c, center_h+width_c, period):  
-#     SLM[i:i + half_period,center_s - width_c:center_s+width_c] = 0 
-#     SLM[center - width_c:center + width_c, i:i + half_period] = 0
-# plt.imsave(f"SLM p_{period}.png", SLM,cmap='gray')
-
-# SLM_fft=fftshift(fft2(SLM))
-# SLM_mag=abs(SLM_fft)/np.max(abs(SLM_fft))
-# plt.imsave(f"FFT_SLM p_{period}.png", SLM_mag,cmap='hot')
-# Display the result
-# Visualize the real and imaginary parts of the phase grating
-plt.figure()
-plt.imshow(np.real(phase_grating), cmap='gray')
-plt.title('Real part of the Phase Grating')
-plt.colorbar()
-plt.show()
 
 plt.figure()
-plt.imshow(np.imag(phase_grating), cmap='gray')
-plt.title('Imaginary part of the Phase Grating')
+plt.imshow(Guassia_amplitude, cmap='hot')
+plt.title('Beam')
 plt.colorbar()
-plt.show()
+plt.imsave(f"Gaussian Beam width {beam_width}m.png",Guassia_amplitude,cmap="hot")
 
 plt.figure()
-plt.imshow(fft_magnitude_cropped, cmap='hot')  # Use log to enhance visibility of low values
+plt.imshow(I_cropped_binary, cmap='hot')  
+plt.title('FFT of the binary phase grating * Beam')
 plt.colorbar()
 plt.show()
+plt.imsave(f"FFT of binary phase grating p_{period/4.6e-6}.png",I_cropped_binary,cmap="hot")
+
 plt.figure()
-plt.imshow(amplitude, cmap='hot')  # Use log to enhance visibility of low values
+plt.imshow(I_cropped_withBlazed, cmap='hot')  
+plt.title('FFT of compensated binary phase grating * Beam')
 plt.colorbar()
 plt.show()
+plt.imsave(f"FFT of compensated binary phase grating pV_{period_Y/4.6e-6} pH_{period_X/4.6e-6}.png",I_cropped_withBlazed,cmap="hot")
+# plt.figure()
+# plt.imshow(I_cropped_binary, cmap='hot')  
+# plt.title('FFT of the binary phase grating')
+
+# # Create x and y tick positions for cropped image
+# # num_ticks = 10  # Number of ticks
+# # xtick_positions = np.linspace(0, I_cropped_binary.shape[1] - 1, num_ticks, dtype=int)
+# # ytick_positions = np.linspace(0, I_cropped_binary.shape[0] - 1, num_ticks, dtype=int)
+# # # Corresponding frequency labels for ticks
+# # xtick_labels = np.round(frequencies_x[start_w:end_w][xtick_positions], 2)
+# # ytick_labels = np.round(frequencies_y[start_h:end_h][ytick_positions], 2)
+
+# # # Set x and y ticks to display spatial frequencies
+# # plt.xticks(xtick_positions, xtick_labels)
+# # plt.yticks(ytick_positions, ytick_labels)
+# plt.colorbar()
+# plt.show()
---
+++ Sim_4_FFT_of_tri/FFTofSLM_blazed.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 11:28:11 2024
+
+@author: SG
+"""
+
+import numpy as np
+from scipy.fft import fft2, fftshift
+import matplotlib.pyplot as plt
+
+
+Rslits=np.ones((1080,1920))
+height,width=Rslits.shape
+
+period_X=92*4.6e-6
+period_Y=400*4.6e-6
+
+x = np.linspace(-0.5*width*4.6e-6, 0.5*width*4.6e-6, width)
+y = np.linspace(-0.5*height*4.6e-6, 0.5*height*4.6e-6, height)
+# x = np.linspace(-0.5*width, 0.5*width, width)
+# y = np.linspace(-0.5*height, 0.5*height, height)
+X, Y = np.meshgrid(x, y)
+# Create a sinusoidal phase grating
+blazed_phase_X = ((2  / period_X) * ((X) %  (period_X)))
+blazed_phase_offset_X=blazed_phase_X
+blazed_grating_X = np.exp(1j *blazed_phase_offset_X*np.pi)#np.mod(blazed_phase, 2 * np.pi)
+
+blazed_phase_Y = ((2 / period_Y) * ((Y) %  (period_Y)))
+blazed_phase_offset_Y=blazed_phase_Y
+blazed_grating_Y = np.exp(1j *blazed_phase_offset_Y*np.pi)
+
+beam_width = 20*4.6e-6
+Guassia_amplitude = np.exp(-((X - 0*4.6e-6)**2 + (Y - 0)**2) / (2 * beam_width**2))
+Guassia_amplitude=Guassia_amplitude/np.max(Guassia_amplitude)
+
+fft_grating_X = fftshift(fft2(blazed_grating_Y*blazed_grating_X*Guassia_amplitude))
+# Compute the magnitude of the Fourier Transform
+I_X = np.abs(fft_grating_X)**2
+I_X =I_X/np.max(I_X)
+# Define the center region to crop
+center_h = height // 2
+center_w = width // 2
+crop_size = 200  # Size of the cropped region (200x200 pixels)
+
+# Crop the FFT magnitude
+start_h = center_h - crop_size // 2
+end_h = center_h + crop_size // 2
+start_w = center_w - crop_size // 2
+end_w = center_w + crop_size // 2
+fft_magnitude_cropped = I_X[start_h:end_h, start_w:end_w]
+
+plt.figure()
+plt.imshow(Guassia_amplitude, cmap='hot')
+plt.title('beam')
+plt.colorbar()
+plt.show()
+
+plt.figure()
+plt.imshow(blazed_phase_Y, cmap='gray')
+plt.title(f'blazed_phase')
+plt.colorbar()
+plt.show()
+
+plt.figure()
+plt.imshow(fft_magnitude_cropped, cmap='hot')  # Use log to enhance visibility of low values
+plt.colorbar()
+plt.show()
