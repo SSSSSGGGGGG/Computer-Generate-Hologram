471be8d36b6e12762d33982dbc8a7acc43ff9bf6
SSSSSGGGGGG <sgao@umh.es>
2025-02-13 16:41:23 +0100

    for TF

---
+++ FFT_CGH_thesis/SNR_Diff/Ch6/IFT_check_circle_TF.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 17:55:01 2024
+
+@author: gaosh
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fftshift,ifft2
+import os
+
+# Read the path of original image.
+original_path2="C:/Users/Laboratorio/MakeHologram/FFT_CGH_thesis/SNR_Diff/Ch6/One circle_1024.png"
+# Read the path of RGB CGHs.
+file_path1 = "C:/Users/Laboratorio/MakeHologram/FFT_CGH_thesis/SNR_Diff/Ch6/One circle_1024_GS_n1_30,n2_5_nl.png"
+file_path2 = "C:/Users/Laboratorio/MakeHologram/FFT_CGH_thesis/SNR_Diff/Ch6/One circle_1024_GS_n1_35,n2_5_nl.png"
+# Automatically retrieve image names
+holo1_name = os.path.basename(file_path1)
+holo2_name = os.path.basename(file_path2)
+# Read RGB CGHs and original image.
+holo1 = plt.imread(file_path1)
+holo2 = plt.imread(file_path2)
+original2=plt.imread(original_path2)
+original2_r=original2[:,:,0]/np.sum(original2[:,:,0])
+original2_g=original2[:,:,1]/np.sum(original2[:,:,1])
+original2_b=original2[:,:,2]/np.sum(original2[:,:,2])
+
+height=holo1.shape[0]
+width=holo1.shape[1]
+# Convert grayscales to angles.
+holo1_R_angle=holo1[:,:,0]*2*np.pi
+holo2_R_angle=holo2[:,:,0]*2*np.pi
+
+holo1_G_angle=holo1[:,:,1]*2*np.pi
+holo2_G_angle=holo2[:,:,1]*2*np.pi
+
+holo1_B_angle=holo1[:,:,2]*2*np.pi
+holo2_B_angle=holo2[:,:,2]*2*np.pi
+# Compute inverse FFT2 of phase holograms to get reconstrutions.
+Reconstruction_holo1_r = fftshift(ifft2(np.exp(1j * holo1_R_angle)))
+Reconstruction_holo2_r = fftshift(ifft2(np.exp(1j * holo2_R_angle)))
+
+Reconstruction_holo1_g = fftshift(ifft2(np.exp(1j * holo1_G_angle)))
+Reconstruction_holo2_g = fftshift(ifft2(np.exp(1j * holo2_G_angle)))
+
+Reconstruction_holo1_b = fftshift(ifft2(np.exp(1j * holo1_B_angle)))
+Reconstruction_holo2_b = fftshift(ifft2(np.exp(1j * holo2_B_angle)))
+
+l=560 
+c_w,c_h=width//2,height//2
+lh,lw=height-2*l,width-2*l # The size of the window.
+
+def hist(r,g,b,name):
+    # Plot histograms for each channel.
+    plt.figure()   
+    plt.hist(r.flatten(), bins=30, color='red', alpha=0.5, label="Red")
+    plt.hist(g.flatten(), bins=30, color='green', alpha=0.5, label="Green")
+    plt.hist(b.flatten(), bins=30, color='blue', alpha=0.5, label="Blue")
+    
+    # Add mean intensity markers.
+    plt.axvline(np.average(r), color='red', linestyle='dashed', linewidth=2, label=f"Mean R={np.average(r):.10f}")
+    plt.axvline(np.average(g), color='green', linestyle='dashed', linewidth=2, label=f"Mean G={np.average(g):.10f}")
+    plt.axvline(np.average(b), color='blue', linestyle='dashed', linewidth=2, label=f"Mean B={np.average(b):.10f}")
+    
+    # Add labels, legend, and title.
+    plt.xlabel('Pixel Intensity')
+    plt.ylabel('Frequency')
+    plt.title(f"{name} RGB Histogram")
+    plt.legend()   
+    # Save the histogram plot
+    plt.savefig(f"{name}_histogram.png")      
+    # Show the plot
+    plt.show()
+# Histgram of the original image in the window.
+O_hist=hist(original2_r[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],original2_g[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],original2_b[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],f"RGB Original")
+# Calculate SNR and Diff for different RGB CGHs, and save the reconstrutions by defined factor, which is designed for visulizing noise.
+def changef(f):
+    factor=f#0.1#1#0.4#0.7#0.025
+       
+    I1_r=np.abs(Reconstruction_holo1_r)**2
+    I1_r_normalized = I1_r / np.sum(I1_r)
+    I1_g=np.abs(Reconstruction_holo1_g)**2
+    I1_g_normalized = I1_g / np.sum(I1_g)
+    I1_b=np.abs(Reconstruction_holo1_b)**2
+    I1_b_normalized = I1_b / np.sum(I1_b)
+    
+    I1_rgb=np.zeros_like(holo1)
+    I1_rgb[:, :, 0] = I1_r_normalized/(np.max(I1_r_normalized)*factor)
+    I1_rgb[:, :, 1] = I1_g_normalized/(np.max(I1_g_normalized)*factor)
+    I1_rgb[:, :, 2] = I1_b_normalized/(np.max(I1_b_normalized)*factor)
+    #SNR in the window.
+    D1_r_snr=np.sum(I1_r[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2])/(np.sum(I1_r)-np.sum(I1_r[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]))
+    D1_g_snr=np.sum(I1_g[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2])/(np.sum(I1_g)-np.sum(I1_g[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]))
+    D1_b_snr=np.sum(I1_b[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2])/(np.sum(I1_b)-np.sum(I1_b[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]))
+    D1_snr=(D1_r_snr+D1_g_snr+D1_b_snr)/3
+    
+    diff_r1=original2_r[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]-I1_r_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]
+    diff_g1=original2_g[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]-I1_g_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]
+    diff_b1=original2_b[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]-I1_b_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]
+    # Diff in the window with respect to the original.
+    D1_r=np.sqrt(np.sum((diff_r1)**2))
+    D1_g=np.sqrt(np.sum((diff_g1)**2))
+    D1_b=np.sqrt(np.sum((diff_b1)**2))
+    D1=(D1_r+D1_g+D1_b)/3
+    # The same calculations for the second RGB CGHs.
+    print(f"For I2 ")
+    I2_r=np.abs(Reconstruction_holo2_r)**2
+    I2_r_normalized = I2_r / np.sum(I2_r)
+    I2_g=np.abs(Reconstruction_holo2_g)**2
+    I2_g_normalized = I2_g / np.sum(I2_g)
+    I2_b=np.abs(Reconstruction_holo2_b)**2
+    I2_b_normalized = I2_b / np.sum(I2_b)
+    
+    I2_rgb_o=np.zeros_like(holo1)
+    I2_rgb_o[:, :, 0] = I2_r_normalized
+    I2_rgb_o[:, :, 1] = I2_g_normalized
+    I2_rgb_o[:, :, 2] = I2_b_normalized
+
+    I2_rgb=np.zeros_like(holo1)
+    I2_rgb[:, :, 0] = I2_r_normalized/(np.max(I2_r_normalized)*factor)
+    I2_rgb[:, :, 1] = I2_g_normalized/(np.max(I2_g_normalized)*factor)
+    I2_rgb[:, :, 2] = I2_b_normalized/(np.max(I2_b_normalized)*factor)
+    #SNR
+    D2_r_snr=np.sum(I2_r[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2])/(np.sum(I2_r)-np.sum(I2_r[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]))
+    D2_g_snr=np.sum(I2_g[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2])/(np.sum(I2_g)-np.sum(I2_g[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]))
+    D2_b_snr=np.sum(I2_b[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2])/(np.sum(I2_b)-np.sum(I2_b[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]))
+    D2_snr=(D2_r_snr+D2_g_snr+D2_b_snr)/3
+    
+    diff_r2=original2_r[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]-I2_r_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]
+    diff_g2=original2_g[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]-I2_g_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]
+    diff_b2=original2_b[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]-I2_b_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2]
+    # Diff
+    D2_r=np.sqrt(np.sum((diff_r2)**2))
+    D2_g=np.sqrt(np.sum((diff_g2)**2))
+    D2_b=np.sqrt(np.sum((diff_b2)**2))
+    D2=(D2_r+D2_g+D2_b)/3
+    print(f"D1_snr { D1_snr}, D2_snr {D2_snr}")
+    print(f"D1 { D1}, D2 {D2}")
+    # Save the correspnding reconstrutions.
+    plt.imsave(f"{holo1_name} f {factor}_win.png", np.clip(I1_rgb, 0, 1))
+    plt.imsave(f"{holo2_name} f {factor}_win.png", np.clip(I2_rgb, 0, 1))
+    # Show the histgrams of reconstrutions in the window.
+    I1_diff_hist=hist(I1_r_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],I1_g_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],I1_b_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],f"{holo1_name}")
+    I2_diff_hist=hist(I2_r_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],I2_g_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],I2_b_normalized[c_h-lh//2:c_h+lh//2, c_w-lw//2:c_w+lw//2],f"{holo2_name}")
+    
+# Call the funtion "changef" by factor "0.05" and "1".
+f2=changef(0.05)
+f3=changef(1)
--- Wirtingers_Holography/FFT_GS_rescale_nc_SNR_sim_TF.py
+++ Wirtingers_Holography/FFT_GS_rescale_nc_SNR_sim_TF.py
@@ -12,20 +12,18 @@ import os
 import cv2
 import time
 
-
 start_t=time.time()
-
 os.chdir("C:/Users/Laboratorio/MakeHologram/Wirtingers_Holography")
-filename="RGB_500" #flowers_tf  RGB 3circles_exp RGB_500 fl_one
+filename="RGB_500" 
 im=plt.imread(f"{filename}.png")
 
 height=im.shape[0]
 width=im.shape[1]
-
+"""Rescaling is only applied for generating the hologams for experiment."""
 # Define wavelengths (in meters, for rescale)
 lambda_r = 0.662e-6  # Red wavelength
 lambda_g = 0.518e-6  # Green wavelength (reference)
-lambda_b = 0.449e-6    # Blue wavelength
+lambda_b = 0.449e-6  # Blue wavelength
 # Calculate scaling factors with respect to green
 scale_r =  lambda_r/lambda_g
 scale_b =  lambda_b/lambda_g
@@ -37,14 +35,12 @@ print(h_b,int(h_b), w_b,int(w_b))
 
 x_offset_r = (int(w_r)-width) // 2
 y_offset_r = (int(h_r)-height) // 2
-print(x_offset_r,y_offset_r)
+
 x_offset_b = (width - int(w_b)) // 2
 y_offset_b = (height - int(h_b)) // 2
-print(x_offset_b,y_offset_b)
 # Step 1: Pad the red channel to the scaled size
 padded_red = np.zeros((h_r, w_r))
 padded_red[y_offset_r:y_offset_r + height, x_offset_r:x_offset_r + width] = im[:, :, 0]
-
 # Step 2: Crop the blue channel to the scaled size
 cropped_blue=np.zeros((h_b, w_b))
 cropped_blue = im[:, :, 2][y_offset_b:y_offset_b + h_b, x_offset_b:x_offset_b + w_b]
@@ -54,85 +50,70 @@ padded_B[y_offset_r:y_offset_r + height, x_offset_r:x_offset_r + width] = im[:, 
 # Step 3: Resize both channels back to the original dimensions
 scaled_red = cv2.resize(padded_red, (width, height), interpolation=cv2.INTER_LINEAR)
 scaled_blue = cv2.resize(cropped_blue, (width, height), interpolation=cv2.INTER_LINEAR)
-
-im_manipulated=np.zeros((height,width,3))
-im_manipulated[:,:,0]=scaled_red
-im_manipulated[:,:,1]=im[:,:,1]
-im_manipulated[:,:,2]=scaled_blue#scaled_blue
-# plt.figure()
-# plt.imshow(im_manipulated[:,:,0])
-# plt.show()
-# plt.figure()
-# plt.imshow(im_manipulated[:,:,1])
-# plt.show()
-# plt.figure()
-# plt.imshow(im_manipulated[:,:,2])
-# plt.show()
-l=620#390
-c_w,c_h=width//2,height//2
-factor=1
-power1=4
+#<<----------------------End of rescaling preparation-------------------------------->>
+power1=4 # Power control
+# Power controlled intensity for different channels. For the simulation, we only need to change the rescaled red and blue channels to unrescaled channels.
 #R
 im_shift_r=fftshift(scaled_red**power1)
 #G
 im_shift_g=fftshift(im[:,:,1]**power1)
 #B
 im_shift_b=fftshift(scaled_blue**power1)
-
-# random
+# Genaration of random phase noise.
 rand=np.random.uniform(0, 1, (height, width))
 rand_2pi=np.pi*rand
 rand_ma=np.max(rand_2pi)
 rand_mi=np.min(rand_2pi)
 exp_rand=np.exp(1j*rand_2pi)
-#R
+# Multiply noise with the field of the preprocessed orignal objects.
 im_r_rand=exp_rand*np.sqrt(im_shift_r)
 
 im_g_rand=exp_rand*np.sqrt(im_shift_g)
 
 im_b_rand=exp_rand*np.sqrt(im_shift_b)
-
-current_field_r = fftshift(fft2(im_r_rand ))
+# Forward FFT of different channels
+current_field_r =fftshift(fft2(im_r_rand ))
 current_field_g =fftshift(fft2(im_g_rand))
 current_field_b =fftshift(fft2(im_b_rand))
 
-iterations1=20
-iterations2=20
+iterations1=20 # Iteration number for the GS without window.
+iterations2=20 # Iteration number for the GS with window.
 
-factor=1
-for j in range(iterations1):
-    
-    # Inverse Fourier Transform to initial plane
+# Loop for the GS without window.
+for j in range(iterations1): 
+    # Reconstrution field that is computed by Inverse FFT2 of the phase value from the previous forward FFT2 computation, and the amplitudes are 1.
     current_field_r_i = ifft2(ifftshift(np.exp(1j * np.angle(current_field_r))))
     current_field_g_i = ifft2(ifftshift(np.exp(1j * np.angle(current_field_g))))
-    current_field_b_i = ifft2(ifftshift(np.exp(1j * np.angle(current_field_b))))
-    
-    
-    current_field_r_n =np.sqrt(im_shift_r)*np.exp(1j * np.angle(current_field_r_i))#*exp_rand
-    current_field_g_n =np.sqrt(im_shift_g)*np.exp(1j * np.angle(current_field_g_i))#*exp_rand
-    current_field_b_n =np.sqrt(im_shift_b)*np.exp(1j * np.angle(current_field_b_i))#*exp_rand
-    
-    # Forward Fourier Transform to the target plane
+    current_field_b_i = ifft2(ifftshift(np.exp(1j * np.angle(current_field_b))))  
+    # Create new reconstrution fields, where the amplitudes are original object fields.
+    current_field_r_n =np.sqrt(im_shift_r)*np.exp(1j * np.angle(current_field_r_i))
+    current_field_g_n =np.sqrt(im_shift_g)*np.exp(1j * np.angle(current_field_g_i))
+    current_field_b_n =np.sqrt(im_shift_b)*np.exp(1j * np.angle(current_field_b_i))
+    # Compute Forward FFT2 to obtain target phase holograms.
     current_field_r = fftshift(fft2(current_field_r_n ))
     current_field_g = fftshift(fft2(current_field_g_n ))
     current_field_b = fftshift(fft2(current_field_b_n ))
-    
-for i in range(iterations2):
-    
-    # Inverse Fourier Transform to initial plane
+# Loop for the GS with window.    
+for i in range(iterations2): 
+    # Inverse FFT2 computation that is the same as the GS without window loop.
     current_field_r_i = ifft2(ifftshift(np.exp(1j * np.angle(current_field_r))))
     current_field_g_i = ifft2(ifftshift(np.exp(1j * np.angle(current_field_g))))
     current_field_b_i = ifft2(ifftshift(np.exp(1j * np.angle(current_field_b))))
-    
+    # Retrieve both amplitudes and phase of inverse FFT2 computation, and normalize amplitude part by the maximum in each channel.
     current_field_r_i_t =np.sqrt(abs(current_field_r_i)**2/np.max(abs(current_field_r_i)**2)) *np.exp(1j * np.angle(current_field_r_i))
     current_field_g_i_t = np.sqrt(abs(current_field_g_i)**2/np.max(abs(current_field_g_i)**2)) *np.exp(1j * np.angle(current_field_g_i))
     current_field_b_i_t =np.sqrt(abs(current_field_b_i)**2/np.max(abs(current_field_b_i)**2)) *np.exp(1j * np.angle(current_field_b_i))
-    
+    # Create new object fields with original fields, and the phases are the same as the previous step.
     current_field_r_n =np.sqrt(im_shift_r)*np.exp(1j * np.angle(current_field_r_i))#*exp_rand
     current_field_g_n =np.sqrt(im_shift_g)*np.exp(1j * np.angle(current_field_g_i))#*exp_rand
     current_field_b_n =np.sqrt(im_shift_b)*np.exp(1j * np.angle(current_field_b_i))#*exp_rand
-    lr=690#int(l*scale_r)
-    lb=558#int(l*scale_b)
+    
+    l=620 # Define widow size, which is the length from window edge to image edge.
+    c_w,c_h=width//2,height//2
+    # For experiment, rescale the window size for red and blue channels.
+    lr=int((height*(scale_r-1)+2*l)/(2*scale_r))
+    lb=int((height*(scale_b-1)+2*l)/(2*scale_b))
+    # In the window, the amplitudes are from original. Otherwise, the amplitudes are from retrieved amplitudes in inverse FFT2 computation.
     current_field_r_n[:,c_w-lr:c_w+lr]=current_field_r_i_t[:,c_w-lr:c_w+lr]
     current_field_g_n[:,c_w-l:c_w+l]=current_field_g_i_t[:,c_w-l:c_w+l]
     current_field_b_n[:,c_w-lb:c_w+lb]=current_field_b_i_t[:,c_w-lb:c_w+lb]
@@ -143,67 +124,51 @@ for i in range(iterations2):
     
     current_field_r_n[c_h-lr:c_h+lr,c_w+lr:width]=current_field_r_i_t[c_h-lr:c_h+lr,c_w+lr:width]
     current_field_g_n[c_h-l:c_h+l,c_w+l:width]=current_field_g_i_t[c_h-l:c_h+l,c_w+l:width]
-    current_field_b_n[c_h-lb:c_h+lb,c_w+lb:width]=current_field_b_i_t[c_h-lb:c_h+lb,c_w+lb:width]
-    
-    # Forward Fourier Transform to the target plane
+    current_field_b_n[c_h-lb:c_h+lb,c_w+lb:width]=current_field_b_i_t[c_h-lb:c_h+lb,c_w+lb:width]    
+    # Compute Forward FFT2 to obtain phase holograms with window.
     current_field_r = fftshift(fft2(current_field_r_n ))
     current_field_g = fftshift(fft2(current_field_g_n ))
     current_field_b = fftshift(fft2(current_field_b_n ))
 
-rdp,gdp,bdp=1.78,2.56,3.5#1.85,2.63,3.55 #1.78,2.56,3.5
-
-# Final optimized phase for display or application on SLM
+# Phase modulation depth for experiment, and they are all 2 for simulation.
+rdp,gdp,bdp=1.78,2.56,3.5
+# Final optimized phase for encoding on SLM, turn angles to grayscales.
 optimized_phase_r = np.angle(current_field_r)
 phase_rr_modi=(optimized_phase_r/np.pi+1)*(255/rdp)
-# phase_rr_modi_mod=np.mod(phase_rr_modi,255)
 
 optimized_phase_g = np.angle(current_field_g)
 phase_gr_modi=(optimized_phase_g/np.pi+1)*(255/gdp)
-# phase_gr_modi_mod=np.mod(phase_gr_modi,255)
 
 optimized_phase_b = np.angle(current_field_b)
 phase_br_modi=(optimized_phase_b/np.pi+1)*(255/bdp)
-# phase_br_modi_mod=np.mod(phase_br_modi,255)
 
 """Lens"""
-lambda_r = 0.662e-6  # Red wavelength
-lambda_g = 0.518e-6  # Green wavelength (reference)
+lambda_r = 0.662e-6  
+lambda_g = 0.518e-6  
 lambda_b = 0.449e-6  
 arr_r=np.zeros((height, width))
 arr_g=np.zeros((height, width))
 arr_b=np.zeros((height, width))
-pixel_size=4.5e-6#4.5e-6
-f=-2 # meters
+pixel_size=4.5e-6
+f=-2 # focal length (meters).
 center_h=height//2
 center_w=width//2
-"""RGB lens"""
+# Calculate the phase of lenses according to Eq. (9.1) for three wavelengths.
 for i in range(height):
     for j in range(width):
         r = pixel_size * np.sqrt((i - center_h)**2 + (j - center_w)**2)
-        arr_r[i, j] =  r**2 / (f * lambda_r) #np.pi *
+        arr_r[i, j] =  r**2 / (f * lambda_r) 
         arr_g[i, j] =  r**2 / (f * lambda_g)
         arr_b[i, j] =  r**2 / (f * lambda_b)
-"""mod into 0-2"""
+# Change the range of the phase into [0,2].
 arr_r_mod=np.mod(arr_r,2)
 arr_g_mod=np.mod(arr_g,2)
-arr_b_mod=np.mod(arr_b,2)    
-
-"""Map phase to gray level for diff laser"""
-arr_r_modified=arr_r_mod*(255/rdp)#1.78)
-arr_g_modified=arr_g_mod*(255/gdp)#2.56)
-arr_b_modified=arr_b_mod*(255/bdp)#3.5)
-# Create a new array for the new image with the same shape as the original
-
-# Assuming phase_rr_modi and arr_r_modified are already defined and have matching shapes
-im_modify_r = np.zeros_like(im, shape=(im.shape[0], im.shape[1],3))
-im_modify_g = np.zeros_like(im, shape=(im.shape[0], im.shape[1],3))
-im_modify_b = np.zeros_like(im, shape=(im.shape[0], im.shape[1],3))
-
-# Fill each channel with respective modifications
-im_modify_r[:,:,0] = phase_rr_modi + arr_r_modified
-im_modify_g[:,:,1]= phase_gr_modi + arr_g_modified
-im_modify_b[:,:,2] = phase_br_modi + arr_b_modified
-
+arr_b_mod=np.mod(arr_b,2)
+# Map the phase to grayscale by corresponding phase modulation.
+arr_r_modified=arr_r_mod*(255/rdp)
+arr_g_modified=arr_g_mod*(255/gdp)
+arr_b_modified=arr_b_mod*(255/bdp)
+# Save and crop the corresponding RGB CGHs with/without lens for experiment and simulation, respectively.
 im_modify_noL = np.zeros_like(im,shape=(im.shape[0], im.shape[1], 3))
 im_modify_noL[:,:,0] = phase_rr_modi
 im_modify_noL[:,:,1] = phase_gr_modi
@@ -220,14 +185,9 @@ def crop(im_modify,name):
     im_cropped = im_cropped.astype(np.uint8)
     im_modi = Image.fromarray(im_cropped)
     im_modi.save(f"{filename}_GS_It1_{iterations1}_It2_{iterations2}_{name}_p {power1}.png")
-# R=crop(im_modify_r, "r")
-# G=crop(im_modify_g, "g")
-# B=crop(im_modify_b, "b")
+
 C=crop(im_modify_c, "L")
-# C_noL=crop(im_modify_noL, "nL")
-# Save each channel separately
-# red_channel.save(f"{filename}_GS_{iterations}_lens_NoCo_HA_r.png")
-# green_channel.save(f"{filename}_GS_{iterations}_lens_NoCo_HA_g.png")
-# blue_channel.save(f"{filename}_GS_{iterations}_lens_NoCo_HA_b.png")
+C_noL=crop(im_modify_noL, "nL")
+
 end_t=time.time()
 print(f"Time consuming {end_t-start_t}s, iteration {iterations1+iterations2}")
