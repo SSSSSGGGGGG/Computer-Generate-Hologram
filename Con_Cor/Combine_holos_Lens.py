bb9cf625863a9f9cfe85e797f18467dbcee81463
SSSSSGGGGGG <sgao@umh.es>
2025-03-17 18:00:21 +0100

    3.17 corr

--- Con_Cor/Combine_holos_Lens.py
+++ Con_Cor/Combine_holos_Lens.py
@@ -35,7 +35,7 @@ for i in range(height):
 arr_r_mod=np.mod(arr_r,2)
 arr_g_mod=np.mod(arr_g,2)
 arr_b_mod=np.mod(arr_b,2)    
-rdp,gdp,bdp=1.78,2.56,3.5
+rdp,gdp,bdp=1.85,2.63,3.55
 """Map phase to gray level for diff laser"""
 arr_r_modified=arr_r_mod*(255/rdp)
 arr_g_modified=arr_g_mod*(255/gdp)
@@ -47,84 +47,94 @@ rgb_image[:, :, 1] = arr_g_modified
 rgb_image[:, :, 2] = arr_b_modified
 arr_r_im=Image.fromarray(rgb_image)
 # arr_r_im.save(f"Lens_rgb{f}.png")
+t="pi"
+im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_Bi(pi)_nL n_1.png")
+im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H0_bi(pi)_nL n_1.png")
+im_ghex=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H180_bi(pi) nL n_1.png")
+# plt.figure()
+# plt.imshow(im_rh)
+# plt.show()
+# plt.figure()
+# plt.imshow(im_gh)
+# plt.show()
+# plt.figure()
+# plt.imshow(im_ghex)
+# plt.show()
+im_rh_array = np.array(im_rh)*255
+im_gh_array = np.array(im_gh)*255
 
-# im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_nL It1_1_It2_0.png")
-# im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H180_nL It1_1_It2_0.png")
+im_new_array = np.zeros_like(im_rh_array)
+im_new_array[:,:,0] = im_rh_array[:,:,0]+im_gh_array[:,:,0]+arr_r_modified
+im_new_array[:,:,1] = im_rh_array[:,:,1]+im_gh_array[:,:,1]+arr_g_modified
+im_new_array[:,:,2] = im_rh_array[:,:,2]+im_gh_array[:,:,2]+arr_b_modified
+
+im_new_array = im_new_array.astype(np.uint8)
+
+im_new = Image.fromarray(im_new_array)
+# im_new.save(f'F_V_H0_bi {t} c.png')
+
+# # im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_nL_(pi) n_1.png")
+# im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H0 r nL_(tri) n_1.png")
+
+# # im_rh_array = np.array(im_rh)*255
+# im_gh_array = np.array(im_gh)*255
+
+# im_new_array = np.zeros_like(im_rh_array)
+# im_new_array[:,:,0] = im_rh_array[:,:,0]+im_gh_array+arr_r_modified
+# im_new_array[:,:,1] = im_rh_array[:,:,1]+arr_g_modified
+# im_new_array[:,:,2] = im_rh_array[:,:,2]+arr_b_modified
+
+# im_new_array = im_new_array.astype(np.uint8)
+
+# im_new = Image.fromarray(im_new_array)
+# im_new.save(f'F_V_H0_{t}_r.png')
+
+# im_new_array_ex = np.zeros_like(im_rh_array)
+# im_new_array_ex[:,:,0] = im_gh_array+arr_r_modified
+# im_new_array_ex = im_new_array_ex.astype(np.uint8)
+# im_new_ex = Image.fromarray(im_new_array_ex)
+# im_new_ex.save(f'F_V_H0_{t}_L r.png')
+
+# # im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_nL It1_1_It2_0.png")
+# im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H0 g nL_(tri) n_1.png")
 
 # im_rh_array = np.array(im_rh)*255
 # im_gh_array = np.array(im_gh)*255
 
 # im_new_array = np.zeros_like(im_rh_array)
-# im_new_array[:,:,0] = im_rh_array[:,:,0]+im_gh_array[:,:,0]+arr_r_modified
-# im_new_array[:,:,1] = im_rh_array[:,:,1]+im_gh_array[:,:,1]+arr_g_modified
-# im_new_array[:,:,2] = im_rh_array[:,:,2]+im_gh_array[:,:,2]+arr_b_modified
+# im_new_array[:,:,0] = im_rh_array[:,:,0]+arr_r_modified
+# im_new_array[:,:,1] = im_rh_array[:,:,1]+im_gh_array+arr_g_modified
+# im_new_array[:,:,2] = im_rh_array[:,:,2]+arr_b_modified
 
 # im_new_array = im_new_array.astype(np.uint8)
 
 # im_new = Image.fromarray(im_new_array)
-# im_new.save('F_V_H180_c.png')
+# im_new.save(f'F_V_H0_{t}_g.png')
 
-im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_nL It1_1_It2_0.png")
-im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H180 r nL It1_1_It2_0.png")
+# im_new_array_ex = np.zeros_like(im_rh_array)
+# im_new_array_ex[:,:,1] = im_gh_array+arr_g_modified
+# im_new_array_ex = im_new_array_ex.astype(np.uint8)
+# im_new_ex = Image.fromarray(im_new_array_ex)
+# im_new_ex.save(f'F_V_H0_{t}_L g.png')
 
-im_rh_array = np.array(im_rh)*255
-im_gh_array = np.array(im_gh)*255
+# # im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_nL It1_1_It2_0.png")
+# im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H0 b nL_(tri) n_1.png")
 
-im_new_array = np.zeros_like(im_rh_array)
-im_new_array[:,:,0] = im_rh_array[:,:,0]+im_gh_array+arr_r_modified
-im_new_array[:,:,1] = im_rh_array[:,:,1]+arr_g_modified
-im_new_array[:,:,2] = im_rh_array[:,:,2]+arr_b_modified
-
-im_new_array = im_new_array.astype(np.uint8)
-
-im_new = Image.fromarray(im_new_array)
-im_new.save('F_V_H180_r.png')
-
-# im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_nL It1_1_It2_0.png")
-# im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H180 r nL It1_1_It2_0.png")
-
-# im_rh_array = np.array(im_rh)*255
+# # im_rh_array = np.array(im_rh)*255
 # im_gh_array = np.array(im_gh)*255
 
-im_new_array = np.zeros_like(im_rh_array)
-im_new_array[:,:,0] = im_rh_array[:,:,0]+arr_r_modified
-im_new_array[:,:,1] = im_rh_array[:,:,1]+im_gh_array+arr_g_modified
-im_new_array[:,:,2] = im_rh_array[:,:,2]+arr_b_modified
+# im_new_array = np.zeros_like(im_rh_array)
+# im_new_array[:,:,0] = im_rh_array[:,:,0]+arr_r_modified
+# im_new_array[:,:,1] = im_rh_array[:,:,1]+arr_g_modified
+# im_new_array[:,:,2] = im_rh_array[:,:,2]+im_gh_array+arr_b_modified
 
-im_new_array = im_new_array.astype(np.uint8)
+# im_new_array = im_new_array.astype(np.uint8)
 
-im_new = Image.fromarray(im_new_array)
-im_new.save('F_V_H180_g.png')
+# im_new = Image.fromarray(im_new_array)
+# im_new.save(f'F_V_H0_{t}_b.png')
 
-# im_rh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_V_nL It1_1_It2_0.png")
-# im_gh=plt.imread("C:/Users/Laboratorio/MakeHologram/Con_Cor/F_H180 r nL It1_1_It2_0.png")
-
-# im_rh_array = np.array(im_rh)*255
-# im_gh_array = np.array(im_gh)*255
-
-im_new_array = np.zeros_like(im_rh_array)
-im_new_array[:,:,0] = im_rh_array[:,:,0]+arr_r_modified
-im_new_array[:,:,1] = im_rh_array[:,:,1]+arr_g_modified
-im_new_array[:,:,2] = im_rh_array[:,:,2]+im_gh_array+arr_b_modified
-
-im_new_array = im_new_array.astype(np.uint8)
-
-im_new = Image.fromarray(im_new_array)
-im_new.save('F_V_H180_b.png')
-
-# im_new.show()
-# # Display results
-# plt.figure()
-# plt.imshow(I_lens,cmap="hot")
-# plt.colorbar()#label='Phase (radians)'
-# plt.axis('off')
-# plt.title("FFT of Lens for red laser")
-# plt.show()
-
-# # # Display results
-# plt.figure()
-# plt.imshow(arr_g_mod,cmap="Greens")
-# plt.colorbar()#label='Phase (radians)'
-# plt.axis('off')
-# plt.title("Lens for red laser im")
-# plt.show()
+# im_new_array_ex = np.zeros_like(im_rh_array)
+# im_new_array_ex[:,:,2] = im_gh_array+arr_b_modified
+# im_new_array_ex = im_new_array_ex.astype(np.uint8)
+# im_new_ex = Image.fromarray(im_new_array_ex)
+# im_new_ex.save(f'F_V_H0_{t}_L b.png')
--- Con_Cor/FFT_SameNoise1.py
+++ Con_Cor/FFT_SameNoise1.py
@@ -63,7 +63,7 @@ y_offset_r = (int(h_r)-height) // 2
 x_offset_b = (width - int(w_b)) // 2
 y_offset_b = (height - int(h_b)) // 2
 
-def FFT(im):
+def rescale(im):
     # Step 1: Pad the red channel to the scaled size
     padded_red = np.zeros((h_r, w_r), dtype=complex)
     padded_red[y_offset_r:y_offset_r + height, x_offset_r:x_offset_r + width] = im[:, :, 0]
@@ -85,6 +85,10 @@ def FFT(im):
     #B
     im_shift_b=fftshift(scaled_blue)
     
+    return im_shift_r,im_shift_g,im_shift_b
+
+rdp,gdp,bdp=1.85,2.63,3.55
+def FFT(im_shift_r,im_shift_g,im_shift_b):    
     im_r_rand=np.sqrt(im_shift_r)
     im_g_rand=np.sqrt(im_shift_g)
     im_b_rand=np.sqrt(im_shift_b)
@@ -93,7 +97,7 @@ def FFT(im):
     current_field_g =fftshift(fft2(im_g_rand))
     current_field_b =fftshift(fft2(im_b_rand))  
     # Phase modulation depth for experiment.
-    rdp,gdp,bdp=1.85,2.63,3.55
+    
     # Convert phase to grayscale.
     optimized_phase_r = np.angle(current_field_r)
     phase_rr_modi=(optimized_phase_r/np.pi+1)*(255/rdp)
@@ -109,101 +113,103 @@ def FFT(im):
     gr,gg,gb=185,185,185
     optimized_phase_r_bi = np.where(optimized_phase_r < 0, 0, 1)
     phase_rr_modi_bi=optimized_phase_r_bi*gr
-   
+       
     optimized_phase_g_bi = np.where(optimized_phase_g < 0, 0, 1)
     phase_gr_modi_bi=optimized_phase_g_bi*gg
     
     optimized_phase_b_bi = np.where(optimized_phase_b < 0, 0, 1)
     phase_br_modi_bi=optimized_phase_b_bi*gb
     
-    """Lens"""
-    lambda_r = 0.662e-6  
-    lambda_g = 0.518e-6  
-    lambda_b = 0.449e-6  
-    arr_r=np.zeros((height, width))
-    arr_g=np.zeros((height, width))
-    arr_b=np.zeros((height, width))
-    pixel_size=4.5e-6
-    f=-2 # focal length (meters).
-    
-    # Calculate the phase of lenses.
-    for i in range(height):
-        for j in range(width):
-            r = pixel_size * np.sqrt((i - center_h)**2 + (j - center_w)**2)
-            arr_r[i, j] =  r**2 / (f * lambda_r) 
-            arr_g[i, j] =  r**2 / (f * lambda_g)
-            arr_b[i, j] =  r**2 / (f * lambda_b)
-    # Change the range of the phase into [0,2].
-    arr_r_mod=np.mod(arr_r,2)
-    arr_g_mod=np.mod(arr_g,2)
-    arr_b_mod=np.mod(arr_b,2)
-    # Map the phase to grayscale by corresponding phase modulation.
-    arr_r_modified=arr_r_mod*(255/rdp)
-    arr_g_modified=arr_g_mod*(255/gdp)
-    arr_b_modified=arr_b_mod*(255/bdp)
-    # Save and crop the corresponding RGB CGHs with/without lens for experiment and simulation, respectively.
-    im_modify_noL = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.float32)
-    im_modify_noL[:,:,0] = phase_rr_modi
-    im_modify_noL[:,:,1] = phase_gr_modi
-    im_modify_noL[:,:,2] = phase_br_modi
-    
-    im_modify_c = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.float32)
-    im_modify_c[:,:,0] = phase_rr_modi+arr_r_modified
-    im_modify_c[:,:,1] = phase_gr_modi+arr_g_modified
-    im_modify_c[:,:,2] = phase_br_modi+arr_b_modified
-    
-    im_modify_c_r = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.float32)
-    im_modify_c_r[:,:,0] = phase_rr_modi+arr_r_modified
-    im_modify_c_g = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.float32)
-    im_modify_c_g[:,:,1] = phase_gr_modi+arr_g_modified
-    im_modify_c_b = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.float32)
-    im_modify_c_b[:,:,2] = phase_br_modi+arr_b_modified
-    
-    im_modify_noL_bi = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.float32)
-    im_modify_noL_bi[:,:,0] = phase_rr_modi_bi
-    im_modify_noL_bi[:,:,1] = phase_gr_modi_bi
-    im_modify_noL_bi[:,:,2] = phase_br_modi_bi
-    
-    im_modify_c_bi = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.float32)
-    im_modify_c_bi[:,:,0] = phase_rr_modi_bi+arr_r_modified
-    im_modify_c_bi[:,:,1] = phase_gr_modi_bi+arr_g_modified
-    im_modify_c_bi[:,:,2] = phase_br_modi_bi+arr_b_modified
-    
-    return im_modify_noL, im_modify_c,im_modify_noL_bi,im_modify_c_bi,im_modify_c_r,im_modify_c_g,im_modify_c_b,type_of
+    return phase_rr_modi,phase_gr_modi,phase_br_modi,phase_rr_modi_bi,phase_gr_modi_bi,phase_br_modi_bi
 
-def crop(im_modify,name):
-    y_offset=center_h-1080//2
-    im_cropped=im_modify[y_offset:y_offset+1080,:]
-    im_cropped = im_cropped.astype(np.uint8)
-    im_modi = Image.fromarray(im_cropped)
-    im_modi.save(f"{name} n_1.png")
-imnl,imc,imnl_bi,imc_bi,imc_r,imc_g,imc_b,t=FFT(im_v)
-# C=crop(imc, f"F_V_L_({t})")
-# C_noL=crop(imnl, f"F_V_nL_({t})")
-C=crop(imc_bi, f"F_V_Bi({t})_L")
-C_noL=crop(imnl_bi, f"F_V_Bi({t})_nL")
+"""Lens"""
+lambda_r = 0.662e-6  
+lambda_g = 0.518e-6  
+lambda_b = 0.449e-6  
+arr_r=np.zeros((height, width))
+arr_g=np.zeros((height, width))
+arr_b=np.zeros((height, width))
+pixel_size=4.5e-6
+f=-2 # focal length (meters).
 
-imnl,imc,imnl_bi,imc_bi,imc_r,imc_g,imc_b,t=FFT(im_l)
-# C=crop(imc, f"F_H0_L_({t})")
-# C_noL=crop(imnl, f"F_H0_nL_({t})")
-# C_noL=crop(imnl[:,:,0], f"F_H0 r nL_({t})")
-# C_noL=crop(imnl[:,:,1], f"F_H0 g nL_({t})")
-# C_noL=crop(imnl[:,:,2], f"F_H0 b nL_({t})")
+# Calculate the phase of lenses.
+for i in range(height):
+    for j in range(width):
+        r = pixel_size * np.sqrt((i - center_h)**2 + (j - center_w)**2)
+        arr_r[i, j] =  r**2 / (f * lambda_r) 
+        arr_g[i, j] =  r**2 / (f * lambda_g)
+        arr_b[i, j] =  r**2 / (f * lambda_b)
+# Change the range of the phase into [0,2].
+arr_r_mod=np.mod(arr_r,2)
+arr_g_mod=np.mod(arr_g,2)
+arr_b_mod=np.mod(arr_b,2)
+# Map the phase to grayscale by corresponding phase modulation.
+arr_r_modified=arr_r_mod*(255/rdp)
+arr_g_modified=arr_g_mod*(255/gdp)
+arr_b_modified=arr_b_mod*(255/bdp)
+# # Save and crop the corresponding RGB CGHs with/without lens for experiment and simulation, respectively.
+# im_modify_noL = np.zeros((height, width, 3), dtype=np.float32)
+# im_modify_noL[:,:,0] = phase_rr_modi
+# im_modify_noL[:,:,1] = phase_gr_modi
+# im_modify_noL[:,:,2] = phase_br_modi
 
-C=crop(imc_bi, f"F_H0__bi({t}) L")
-C_noL=crop(imnl_bi, f"F_H0_bi({t})_nL")
+# im_modify_c = np.zeros((height, width, 3), dtype=np.float32)
+# im_modify_c[:,:,0] = phase_rr_modi+arr_r_modified
+# im_modify_c[:,:,1] = phase_gr_modi+arr_g_modified
+# im_modify_c[:,:,2] = phase_br_modi+arr_b_modified
 
-imnl,imc,imnl_bi,imc_bi,imc_r,imc_g,imc_b,t=FFT(im_l_fl)
-# C=crop(imc, f"F_H180_L_({t})")
-# C_noL=crop(imnl, f"F_H180_nL_({t})")
-# C_noL=crop(imnl[:,:,0], f"F_H180 r nL_({t})")
-# C_noL=crop(imnl[:,:,1], f"F_H180 g nL_({t})")
-# C_noL=crop(imnl[:,:,2], f"F_H180 b nL_({t})")
-# C_noL=crop(imc_r, f"F_H180 r L_({t})")
-# C_noL=crop(imc_g, f"F_H180 g L_({t})")
-# C_noL=crop(imc_b, f"F_H180 b L_({t})")
-C=crop(imc_bi,f"F_H180_bi({t}) L")
-C_noL=crop(imnl_bi, f"F_H180_bi({t}) nL")
+# im_modify_c_r = np.zeros((height, width, 3), dtype=np.float32)
+# im_modify_c_r[:,:,0] = phase_rr_modi+arr_r_modified
+# im_modify_c_g = np.zeros((height, width, 3), dtype=np.float32)
+# im_modify_c_g[:,:,1] = phase_gr_modi+arr_g_modified
+# im_modify_c_b = np.zeros((height, width, 3), dtype=np.float32)
+# im_modify_c_b[:,:,2] = phase_br_modi+arr_b_modified
 
-end_t=time.time()
-print(f"Time consuming {end_t-start_t}s")
+# im_modify_noL_bi = np.zeros((height, width, 3), dtype=np.float32)
+# im_modify_noL_bi[:,:,0] = phase_rr_modi_bi
+# im_modify_noL_bi[:,:,1] = phase_gr_modi_bi
+# im_modify_noL_bi[:,:,2] = phase_br_modi_bi
+
+# im_modify_c_bi = np.zeros((height, width, 3), dtype=np.float32)
+# im_modify_c_bi[:,:,0] = phase_rr_modi_bi+arr_r_modified
+# im_modify_c_bi[:,:,1] = phase_gr_modi_bi+arr_g_modified
+# im_modify_c_bi[:,:,2] = phase_br_modi_bi+arr_b_modified
+
+# return im_modify_noL, im_modify_c,im_modify_noL_bi,im_modify_c_bi,im_modify_c_r,im_modify_c_g,im_modify_c_b,type_of
+
+# def crop(im_modify,name):
+#     y_offset=center_h-1080//2
+#     im_cropped=im_modify[y_offset:y_offset+1080,:]
+#     im_cropped = im_cropped.astype(np.uint8)
+#     im_modi = Image.fromarray(im_cropped)
+#     im_modi.save(f"{name} n_1.png")
+# imnl,imc,imnl_bi,imc_bi,imc_r,imc_g,imc_b,t=FFT(im_v)
+# # C=crop(imc, f"F_V_L_({t})")
+# # C_noL=crop(imnl, f"F_V_nL_({t})")
+# C=crop(imc_bi, f"F_V_Bi({t})_L")
+# C_noL=crop(imnl_bi, f"F_V_Bi({t})_nL")
+
+# imnl,imc,imnl_bi,imc_bi,imc_r,imc_g,imc_b,t=FFT(im_l)
+# # C=crop(imc, f"F_H0_L_({t})")
+# # C_noL=crop(imnl, f"F_H0_nL_({t})")
+# # C_noL=crop(imnl[:,:,0], f"F_H0 r nL_({t})")
+# # C_noL=crop(imnl[:,:,1], f"F_H0 g nL_({t})")
+# # C_noL=crop(imnl[:,:,2], f"F_H0 b nL_({t})")
+
+# C=crop(imc_bi, f"F_H0__bi({t}) L")
+# C_noL=crop(imnl_bi, f"F_H0_bi({t})_nL")
+
+# imnl,imc,imnl_bi,imc_bi,imc_r,imc_g,imc_b,t=FFT(im_l_fl)
+# # C=crop(imc, f"F_H180_L_({t})")
+# # C_noL=crop(imnl, f"F_H180_nL_({t})")
+# # C_noL=crop(imnl[:,:,0], f"F_H180 r nL_({t})")
+# # C_noL=crop(imnl[:,:,1], f"F_H180 g nL_({t})")
+# # C_noL=crop(imnl[:,:,2], f"F_H180 b nL_({t})")
+# # C_noL=crop(imc_r, f"F_H180 r L_({t})")
+# # C_noL=crop(imc_g, f"F_H180 g L_({t})")
+# # C_noL=crop(imc_b, f"F_H180 b L_({t})")
+# C=crop(imc_bi,f"F_H180_bi({t}) L")
+# C_noL=crop(imnl_bi, f"F_H180_bi({t}) nL")
+
+# end_t=time.time()
+# print(f"Time consuming {end_t-start_t}s")
